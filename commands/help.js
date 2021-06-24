8957de3587d78ca34c6a90436f84414d64537717
ahmedkaremeg <76890886+ahmedkaremeg@users.noreply.github.com>
2021-06-24 02:02:36 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,10 +36,9 @@ module.exports = {
               }help [Command] | Have a nice day!`
             ).setDescription(`${Commands.join("\n")}
   
-  Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.config.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | `);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
